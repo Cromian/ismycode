1e26afe88cb105aeb9c171cf53823c7cf14902c3
Cromian <misha.vinokur@gmail.com>
2017-07-20 04:53:41 -0400

    added read json file logic

--- logic.php
+++ logic.php
@@ -1,24 +1,41 @@
 <?php
 
-function showText($emojie_code, $status) {
+// Show text function
+function showText($emojie_code, $status_txt) {
 
     print '<h1>Is my code working today?</h1>';
 
     print '<div class="mood-txt">
             <i class="em ' . $emojie_code . '"></i></br>
-            ' . $status . '
+            ' . $status_txt . '
           </div>';
 
 }
 
+// Status varaible extracted from status.json
+$json_file = file_get_contents('status.json');
+$json_items = json_decode($json_file, true);
+$status = $json_items['status'];
+
+// Logic statements
+if ($status == "yes")
+{
+    showText(
+        $emojie_code = 'em-smile',
+        $status_txt = 'Yes' 
+    );
+}
+
+if ($status == "no")
+{
+    showText(
+        $emojie_code = 'em-angry',
+        $status_txt = 'No' 
+    );
+}
 
 
-showText(
 
-    $emojie_code = 'em-smile',
-    $status = 'Yes' 
-
-);
 
 
 ?>
